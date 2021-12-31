daf87acf25f4c6fc15b8b37722b293398ee357b0
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-12-31 22:51:38 +0600

    Using js math and object  in html in react

--- Module-32-React core concepts,JSx,props,state/my-app/src/App.js
+++ Module-32-React core concepts,JSx,props,state/my-app/src/App.js
@@ -2,6 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const person = {
+    name: 'Asif Ur Rahman',
+    age: 29,
+    job: 'Developer',
+    hobbies: ['Sports', 'Cooking']
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -9,7 +15,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        
+        <p>Starting a new journey with reach</p>
+        <h1>My heading : {2+5}</h1>
+        <h1>My Name:{person.name}</h1>
+        <h1>My Name:{person.job}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
