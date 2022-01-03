f954f84b4d95778ffe7d5f0429622f8ea8c33c65
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-03 21:41:44 +0600

    Loading data from fakeData

--- Module-34-Simple react spa with multiple component/ema-joh-shopping-mall/src/components/Shop/Shop.js
+++ Module-34-Simple react spa with multiple component/ema-joh-shopping-mall/src/components/Shop/Shop.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import { useEffect } from 'react/cjs/react.development';
+import fakeData from '../../fakeData/products.JSON';
 const Shop = () => {
+    const [products, setProducts] = React.useState([]);
+    useEffect(() => {
+        fetch(fakeData)
+        .then(res => res.json())
+        .then(data => {
+            setProducts(data);
+        })
+    }, []);
+    console.log(products.slice(0,10));
     return (
         <div>
             <h1>This is shop</h1>
+            <ul>
+                {products.slice(0,10).map(product => <li>{product.name}</li>)}
+            </ul>
         </div>
     );
 };
