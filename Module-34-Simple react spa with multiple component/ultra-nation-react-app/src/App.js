4e338b9ba852763f3b7ea46654a2b2878a2f47f3
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-04 21:36:51 +0600

    showing all added country in UI

--- Module-34-Simple react spa with multiple component/ultra-nation-react-app/src/App.js
+++ Module-34-Simple react spa with multiple component/ultra-nation-react-app/src/App.js
@@ -3,24 +3,26 @@ import {useState,useEffect} from 'react';
 import Country from './components/Country/Country';
 function App() {
   const [countries, setCountries] = useState([]);
-  let [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
       .then(response => response.json())
       .then(data => setCountries(data));
   },[])
   const addCountries = (country) => {
-    cart = [...cart,country]
-    setCart(cart);
+    setCart([...cart,country]);
   }
   const totalPopulation = cart.reduce((totalPopulation,country) => totalPopulation + country.population,0);
   console.log(cart);
+  const countryAdded = cart.map(country =>country.name.common);
+  console.log(countryAdded);
   return (
     <div>
       <div style={{border:'3px solid lightgray', textAlign:'center'}}>
         <h1>Total countries: {countries.length}</h1>
         <h2>Add countries: {cart.length}</h2>
         <h2>Total population: {totalPopulation}</h2>
+        <h2>Country Added: {countryAdded.join(',')}</h2>
       </div>
       <div style={{display:'grid', gridTemplateColumns: 'auto auto'}}>
         {
