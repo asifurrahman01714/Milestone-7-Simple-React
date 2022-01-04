064eb0b205ba25084916ca45aa172462bb0e7abd
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-04 19:00:48 +0600

    optional chaining

--- Module-34-Simple react spa with multiple component/ultra-nation-react-app/src/components/Country/Country.js
+++ Module-34-Simple react spa with multiple component/ultra-nation-react-app/src/components/Country/Country.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 const Country = (props) => {
-    
-    console.log(props.country);
+    const countryStyle = {
+        border: '2px solid black',
+        borderRadius: '5px',
+        padding: '10px',
+        margin: '10px',
+        textAlign: 'center'
+    }
+    console.log(props.country.capital?.[0]); // this is called optional chaining
     const { name, capital, population, languages, flags } = props.country;
     return (
-        <div>
+        <div style={countryStyle}>
             <img style={{height:'50px'}} src={flags.png} alt="" />
             <h1>Name :{name.common}</h1>
+            <h1>Capital :{capital?.[0]}</h1>
         </div>
     );
 };
