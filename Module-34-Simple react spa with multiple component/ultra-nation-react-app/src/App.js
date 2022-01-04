f21c25aca39dce8196030b27aadd9633d5ab56cf
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-04 17:24:54 +0600

    useState, useEffect

--- Module-34-Simple react spa with multiple component/ultra-nation-react-app/src/App.js
+++ Module-34-Simple react spa with multiple component/ultra-nation-react-app/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import {useState,useEffect} from 'react';
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(response => response.json())
+      .then(data => setCountries(data));
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Total countries: {countries.length}</h1>
     </div>
   );
 }
