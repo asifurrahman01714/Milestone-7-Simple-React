89f58afb3d34d59dfb92610ba0f766965977c85b
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-04 21:29:25 +0600

    You can update state using cart

--- Module-34-Simple react spa with multiple component/ultra-nation-react-app/src/App.js
+++ Module-34-Simple react spa with multiple component/ultra-nation-react-app/src/App.js
@@ -3,23 +3,26 @@ import {useState,useEffect} from 'react';
 import Country from './components/Country/Country';
 function App() {
   const [countries, setCountries] = useState([]);
-  const [cart, setCart] = useState([]);
+  let [cart, setCart] = useState([]);
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
       .then(response => response.json())
       .then(data => setCountries(data));
   },[])
   const addCountries = (country) => {
-    setCart([...cart,country]);
+    cart = [...cart,country]
+    setCart(cart);
   }
   const totalPopulation = cart.reduce((totalPopulation,country) => totalPopulation + country.population,0);
   console.log(cart);
   return (
     <div>
-      <h1>Total countries: {countries.length}</h1>
-      <h2>Add countries: {cart.length}</h2>
-      <h2>Total population: {totalPopulation}</h2>
-      <div style={{display:'grid', gridTemplateColumns: 'auto auto auto auto'}}>
+      <div style={{border:'3px solid lightgray', textAlign:'center'}}>
+        <h1>Total countries: {countries.length}</h1>
+        <h2>Add countries: {cart.length}</h2>
+        <h2>Total population: {totalPopulation}</h2>
+      </div>
+      <div style={{display:'grid', gridTemplateColumns: 'auto auto'}}>
         {
           countries.map(country => <Country addCountries={addCountries} country={country} key={country.name.common}></Country>)
         }
