466560967b85870ada54295f283781c5b712d628
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-05 18:46:07 +0600

    connect player and cart with home

--- Module-34-Simple react spa with multiple component/build-your-own-team/src/components/Home/Home.js
+++ Module-34-Simple react spa with multiple component/build-your-own-team/src/components/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Cart from '../Cart/Cart';
+import Player from '../Player/Player';
 
 const Home = () => {
     return (
         <div style={{marginTop: '10px'}}>
             <div>
-                
+                <Player></Player>
             </div>
-            <div></div>
+            <div>
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
