7b9f5389091bef1d4d1164d58b26ede9b2b9ac4a
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-05 18:38:47 +0600

    Creating navbar using bootstrap

--- Module-34-Simple react spa with multiple component/build-your-own-team/src/components/Header/Header.js
+++ Module-34-Simple react spa with multiple component/build-your-own-team/src/components/Header/Header.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import './Header.css';
 const Header = () => {
+    const inputStyle = {
+        width: '80%',
+        height: '30px',
+        border: '2px solid #8585c1'
+    };
     return (
-        <div>
-            <h1>This is header</h1>
+        <div className="header">
+            <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand">Player</a>
+                <form className="d-flex" style={{width: '80%'}}>
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+            </nav>
         </div>
     );
 };
