46b6a7181e6a2bd1e3ed2eb72e23e4e3ea516cc7
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-05 21:07:46 +0600

    1. currency to number 2. showing name and dynamic table, dynamic salary, total salary total teamplayers

--- Module-34-Simple react spa with multiple component/build-your-own-team/src/components/Cart/Cart.js
+++ Module-34-Simple react spa with multiple component/build-your-own-team/src/components/Cart/Cart.js
@@ -3,12 +3,32 @@ import React from 'react';
 const Cart = (props) => {
     const newPlayers = props.newPlayers;
     // console.log(newPlayers);
+    let total =0;
+    for (let i = 0; i < newPlayers.length; i++) {
+        const element = newPlayers[i].salary;
+        total = total + Number(element.replace(/[^0-9.-]+/g,""));  
+    }
+    
     return (
         <div>
             <h3 style={{textAlign: 'center'}}>Total Players :{newPlayers.length}</h3>
-            {
-                newPlayers.map(player => <p>{player.name} : {player.salary}</p>)
-            }
+            <table className="table table-dark table-hover w-100">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Salary</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        newPlayers.map(player => <tr><th>{player.name}</th><th>{player.salary}</th></tr>)
+                    }
+                    <tr>
+                        <th scope="col">Total Salary: </th>
+                        <th scope="col">{0 || total}</th>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
