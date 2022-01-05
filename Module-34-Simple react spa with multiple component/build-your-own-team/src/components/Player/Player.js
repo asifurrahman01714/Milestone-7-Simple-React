42103bd1540fd059cb8b54a8b27f88b2dbce2b6e
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-05 21:28:17 +0600

    image height in card solved

--- Module-34-Simple react spa with multiple component/build-your-own-team/src/components/Player/Player.js
+++ Module-34-Simple react spa with multiple component/build-your-own-team/src/components/Player/Player.js
@@ -6,9 +6,9 @@ const Player = (props) => {
     const {name, image} = props.player;
     const handleAddPlayer = props.handleAddPlayer;
     return (
-        <div className="col-md-6" style={{marginBottom:'10px'}}>
-            <div className="card" style={{height:'550px'}}>
-                <img src={image} style={{height: '75%'}} className="card-img-top" alt="..."/>
+        <div className="col-md-4" style={{marginBottom:'10px'}}>
+            <div className="card" style={{height:'500px'}}>
+                <img src={image} style={{height: '68%'}} className="card-img-top" alt="..."/>
                 <div className="card-body" style={{textAlign: 'center'}}>
                     <h2 style={{fontWeight: '700', marginBottom:'15px'}} className="card-title">{name}</h2>
                     <a href="#" className="btn btn-primary" onClick={()=>handleAddPlayer(props.player)}>Add me</a>
