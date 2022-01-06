2be41bb9283b7e2b12ef5effd60e7f18697e81b7
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-06 21:03:39 +0600

    show salary with button of any player

--- Module-34-Simple react spa with multiple component/build-your-own-team/src/components/Player/Player.js
+++ Module-34-Simple react spa with multiple component/build-your-own-team/src/components/Player/Player.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { useState } from 'react/cjs/react.development';
 
 const Player = (props) => {
     // console.log(props);
     // console.log(props.player);
     const {name, image} = props.player;
     const handleAddPlayer = props.handleAddPlayer;
+    const [salary, updateSalary] = useState('');
+    const displaySalary = (playerSalary)=>{
+        updateSalary(playerSalary)
+    }
+    console.log('salary', salary);
     return (
         <div className="col-md-4" style={{marginBottom:'10px'}}>
             <div className="card" style={{height:'500px'}}>
                 <img src={image} style={{height: '68%'}} className="card-img-top" alt="..."/>
                 <div className="card-body" style={{textAlign: 'center'}}>
                     <h2 style={{fontWeight: '700', marginBottom:'15px'}} className="card-title">{name}</h2>
-                    <a href="#" className="btn btn-primary" onClick={()=>handleAddPlayer(props.player)}>Add me</a>
+                    <h5 style={{fontWeight: '700', marginBottom:'15px'}} className="card-title">Salary: {salary}</h5>
+                    <button className="btn btn-primary" onClick={()=>handleAddPlayer(props.player)}>Add me</button>
+                    <button className="btn btn-primary" onClick={()=>displaySalary(props.player.salary)}>DisplaySalary</button>
                 </div>
             </div>
         </div>
