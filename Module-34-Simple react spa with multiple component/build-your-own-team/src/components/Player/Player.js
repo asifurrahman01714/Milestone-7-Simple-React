9da1faada95564f6243bff46a1556f5d73b593a7
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-06 21:14:20 +0600

    Update state in same component with event handler on the same component

--- Module-34-Simple react spa with multiple component/build-your-own-team/src/components/Player/Player.js
+++ Module-34-Simple react spa with multiple component/build-your-own-team/src/components/Player/Player.js
@@ -4,22 +4,22 @@ import { useState } from 'react/cjs/react.development';
 const Player = (props) => {
     // console.log(props);
     // console.log(props.player);
-    const {name, image} = props.player;
+    const {name, image, salary} = props.player;
     const handleAddPlayer = props.handleAddPlayer;
-    const [salary, updateSalary] = useState('');
-    const displaySalary = (playerSalary)=>{
-        updateSalary(playerSalary)
-    }
-    console.log('salary', salary);
+    const [playerSalary, updateSalary] = useState('');
+    // const displaySalary = (playerSalary)=>{
+    //     updateSalary(playerSalary)
+    // }
+    const displaySalary = ()=>updateSalary(salary);
     return (
         <div className="col-md-4" style={{marginBottom:'10px'}}>
             <div className="card" style={{height:'500px'}}>
                 <img src={image} style={{height: '68%'}} className="card-img-top" alt="..."/>
                 <div className="card-body" style={{textAlign: 'center'}}>
                     <h2 style={{fontWeight: '700', marginBottom:'15px'}} className="card-title">{name}</h2>
-                    <h5 style={{fontWeight: '700', marginBottom:'15px'}} className="card-title">Salary: {salary}</h5>
+                    <h5 style={{fontWeight: '700', marginBottom:'15px'}} className="card-title">Salary: {playerSalary}</h5>
                     <button className="btn btn-primary me-2" onClick={()=>handleAddPlayer(props.player)}>Add me</button>
-                    <button className="btn btn-primary" onClick={()=>displaySalary(props.player.salary)}>DisplaySalary</button>
+                    <button className="btn btn-primary" onClick={displaySalary}>DisplaySalary</button>
                 </div>
             </div>
         </div>
