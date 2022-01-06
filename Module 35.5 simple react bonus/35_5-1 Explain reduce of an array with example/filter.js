7a6eded28ce1e3e6887945499b74ef72727bc088
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-06 18:55:05 +0600

    see index of any array item

--- Module 35.5 simple react bonus/35_5-1 Explain reduce of an array with example/filter.js
+++ Module 35.5 simple react bonus/35_5-1 Explain reduce of an array with example/filter.js
@@ -1,3 +1,6 @@
-const nums = [10, 20, 30, 40, 50];
+const nums = [10, 20, 30, 40, 50, 10,5,5];
 const result = nums.filter(num => num > 20);
 console.log(result);
+
+const seeIndex = nums.indexOf(5);
+console.log(seeIndex);
