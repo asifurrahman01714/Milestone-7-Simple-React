7ac5ad623a5b58c0700612259b9ac556b9cabc37
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-06 18:36:02 +0600

    find method

--- Module 35.5 simple react bonus/35_5-1 Explain reduce of an array with example/find.js
+++ Module 35.5 simple react bonus/35_5-1 Explain reduce of an array with example/find.js
@@ -1,3 +1,15 @@
 const nums = [10, 20, 30, 40, 50];
 const result = nums.find(num => num > 20);
 console.log(result);
+
+const students = [
+    { id: 1, name: "Tuan" },
+    { id: 2, name: "Tuan2" },
+    { id: 3, name: "Tuan3" },
+    { id: 4, name: "Tuan4" },
+]
+const findStudent1 = students.find(student => student.id === 2);
+console.log(findStudent1);
+
+const findStudent2 = students.find(student => student.name === 'Tuan3');
+console.log(findStudent2);
