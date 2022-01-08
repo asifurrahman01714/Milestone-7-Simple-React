94d7bdbf450019e74775b987aa7c1060a5fa3865
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-08 15:50:42 +0600

    calling product js in product details and passing the selected product

--- Module-34-Simple react spa with multiple component/ema-joh-shopping-mall/src/components/ProductDetails/ProductDetails.js
+++ Module-34-Simple react spa with multiple component/ema-joh-shopping-mall/src/components/ProductDetails/ProductDetails.js
@@ -2,26 +2,27 @@ import React from 'react';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import { useEffect, useState } from 'react/cjs/react.development';
 import fakeData from '../../fakeData/products.JSON';
+import Product from '../Product/Product';
 
 const ProductDetails = () => {
     // console.log(useParams());
     const {productId} = useParams();
-    // const [products, setProducts] = React.useState([]);
-    // useEffect(() => {
-    //     fetch(fakeData)
-    //     .then(res => res.json())
-    //     .then(data => {
-    //         setProducts(data);
-    //     })
-    // }, []);
+    const [products, setProducts] = React.useState([]);
+    useEffect(() => {
+        fetch(fakeData)
+        .then(res => res.json())
+        .then(data => {
+            setProducts(data);
+        })
+    }, []);
     const product = fakeData.find(pd => pd.key === productId);
     // console.log(products);
     console.log(product);
 
     return (
         <div>
-            <h1>Product Id :{product?.key}</h1>
-            <h1>Product Name :{product?.name}</h1>
+            <h1>Product Id :{productId}</h1>
+            <Product product={product}/>
         </div>
     );
 };
