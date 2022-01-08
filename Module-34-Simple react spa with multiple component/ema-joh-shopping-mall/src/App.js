43b643f37f0a55b492d50e2fb882a9cbe3a8f0de
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-08 11:21:57 +0600

    Solved react router switch issue for v5

--- Module-34-Simple react spa with multiple component/ema-joh-shopping-mall/src/App.js
+++ Module-34-Simple react spa with multiple component/ema-joh-shopping-mall/src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Shop from "./components/Shop/Shop";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 function App() {
   return (
     <div>
       <Header />
-      <Shop></Shop>
+      <Router>
+        <Switch>
+          <Route path="/about">
+            <Shop />
+          </Route>
+          
+          <Route exact path="/">
+            <Shop />
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
