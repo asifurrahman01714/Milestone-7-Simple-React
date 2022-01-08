49295099dbb25ccb6a13591d38ee3e9b7a75942b
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-08 15:17:34 +0600

    fetching all data from fakedata json

--- Module-34-Simple react spa with multiple component/ema-joh-shopping-mall/src/components/ProductDetails/ProductDetails.js
+++ Module-34-Simple react spa with multiple component/ema-joh-shopping-mall/src/components/ProductDetails/ProductDetails.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { useEffect, useState } from 'react/cjs/react.development';
+import fakeData from '../../fakeData/products.JSON';
 
 const ProductDetails = () => {
-    console.log(useParams());
+    const [products, setProducts] = React.useState([]);
+    useEffect(() => {
+        fetch(fakeData)
+        .then(res => res.json())
+        .then(data => {
+            setProducts(data);
+        })
+    }, []);
+    // console.log(products);
+    // console.log(useParams());
     const {productId} = useParams();
     return (
         <div>
