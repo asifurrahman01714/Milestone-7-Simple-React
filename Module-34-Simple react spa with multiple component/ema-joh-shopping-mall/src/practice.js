310c8e91c94a30a2f4e063ce73bd836081174069
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-13 20:26:55 +0600

    template string

--- Module-34-Simple react spa with multiple component/ema-joh-shopping-mall/src/practice.js
+++ Module-34-Simple react spa with multiple component/ema-joh-shopping-mall/src/practice.js
@@ -10,3 +10,23 @@ These are constant values and cannot be changed.
 The variable total is declared with the let keyword. 
 This is a value that can be changed.
 */
+// object
+let person = {
+    firstName:"John", 
+    lastName:"Doe", 
+    age:50, 
+    eyeColor:"blue"
+};
+
+/* Template literals are enclosed by the backtick (` `) character 
+instead of double or single quotes.
+
+Template literals can contain placeholders. 
+These are indicated by the dollar sign and curly braces (${expression}). 
+The expressions in the placeholders and the text between the 
+backticks (` `) get passed to a function. 
+Using this template literal we can easily call any property from any object.
+*/
+let text = `Welcome ${person.name} !`;
+console.log(text);
+
