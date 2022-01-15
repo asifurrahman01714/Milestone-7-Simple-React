50f88a1d1f924192f5ad5865dbdd4e85805418fa
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-15 15:45:35 +0600

    Adding style

--- Module-34-Simple react spa with multiple component/ema-joh-shopping-mall/src/components/ReviewItem/ReviewItem.js
+++ Module-34-Simple react spa with multiple component/ema-joh-shopping-mall/src/components/ReviewItem/ReviewItem.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 const ReviewItem = (props) => {
     const {name, quantity, price} = props.product;
+    const reviewItemStyle ={
+        border: '1px solid gray',
+        borderRadius: '5px',
+        backgroundColor: '#f4f4f4',
+        margin: '10px',
+        padding: '10px'
+
+    }
     return (
-        <div>
-            <h1>Name: {name}</h1>
-            <h1>Quantity: {quantity}</h1>
+        <div style={reviewItemStyle}>
+            <h3>Name: {name}</h3>
+            <h4>Quantity: {quantity}</h4>
             <button>Remove</button>
         </div>
     );
