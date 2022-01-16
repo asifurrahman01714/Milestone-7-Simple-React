800e7ef02b68e16a56b6c76401b0666bfa2705da
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-16 10:42:43 +0600

    some style has been changed

--- Module-34-Simple react spa with multiple component/ema-joh-shopping-mall/src/components/Product/Product.js
+++ Module-34-Simple react spa with multiple component/ema-joh-shopping-mall/src/components/Product/Product.js
@@ -21,7 +21,7 @@ const Product = (props) => {
             </div>
             <div className="singleProductContent">
                 {/* <h3><Link to={"/product/"+key}>{name}</Link> </h3> */}
-                <h3><Link to={`/product/${key}`}>{name}</Link> </h3>
+                <h5><Link to={`/product/${key}`}>{name}</Link> </h5>
                 <span>by: {seller}</span>
                 <h3>Price: ${price}</h3>
                 {props.showAddToCart !== false && <button onClick={()=>{props.handleAddProduct(props.pd)}}><FontAwesomeIcon icon={faShoppingCart}/> Add to Cart</button>}
