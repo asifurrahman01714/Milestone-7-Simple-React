8d0be07f65d5e3b571f0cdcd9437f541094afd56
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-17 21:59:15 +0600

    conditional rendering

--- Module-32-React core concepts,JSx,props,state/my-app/src/components/Familiar/Familiar.js
+++ Module-32-React core concepts,JSx,props,state/my-app/src/components/Familiar/Familiar.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const Familiar = () => {
+const Familiar = (props) => {
+    const familiar = props.familiar;
+    let greetings;
+    if (familiar) {
+        greetings = <h1>How are you my friend?</h1>
+    } else {
+        greetings = <h1>Who the hell you are?</h1>
+    }
     return (
         <div>
             <div>
                 <h1>Greetings</h1>
+                
             </div>
         </div>
     );
