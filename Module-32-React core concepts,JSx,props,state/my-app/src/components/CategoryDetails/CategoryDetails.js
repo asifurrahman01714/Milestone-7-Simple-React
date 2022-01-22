916e5dfa88eca264fdfc69a93f7b0733f450d50b
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-22 21:00:27 +0600

    Wow..we have accessed the hardcodded value using useCOntext

--- Module-32-React core concepts,JSx,props,state/my-app/src/components/CategoryDetails/CategoryDetails.js
+++ Module-32-React core concepts,JSx,props,state/my-app/src/components/CategoryDetails/CategoryDetails.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useContext } from 'react/cjs/react.development';
+import { UserContext } from '../../App';
 
 const CategoryDetails = () => {
+    const machine = useContext(UserContext)
     return (
         <div>
-            <h1>Category Details of :</h1>
+            <h1>Category Details of :{machine}</h1>
         </div>
     );
 };
