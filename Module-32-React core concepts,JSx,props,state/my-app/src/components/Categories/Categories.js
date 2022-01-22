82c04549e0bf2a430b035b0f47bd15f68d23b920
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-22 22:22:58 +0600

    creating a fakeData

--- Module-32-React core concepts,JSx,props,state/my-app/src/components/Categories/Categories.js
+++ Module-32-React core concepts,JSx,props,state/my-app/src/components/Categories/Categories.js
@@ -5,6 +5,17 @@ import CategoryDetails from '../CategoryDetails/CategoryDetails';
 
 const Categories = () => {
     const [category, setCategory] = useContext(CategoryContext);
+    const products = [
+        {name:'Asus', category:'Laptop'},
+        {name:'Dell', category:'Laptop'},
+        {name:'HP', category:'Laptop'},
+        {name:'Canon', category:'Camera'},
+        {name:'Nikon', category:'Camera'},
+        {name:'Sony', category:'Camera'},
+        {name:'Samsung', category:'Mobile'},
+        {name:'Apple', category:'Mobile'},
+        {name:'Huawei', category:'Mobile'},
+    ]
     return (
         <div>
             <h1>Category  :{category}</h1>
