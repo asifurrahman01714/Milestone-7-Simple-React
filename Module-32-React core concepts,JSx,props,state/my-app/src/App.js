3f0973b478c2fc0cdd99fe41fcb781125d8fb4d4
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-22 20:24:24 +0600

    Now, all components in this tree will have access to the user Context.

--- Module-32-React core concepts,JSx,props,state/my-app/src/App.js
+++ Module-32-React core concepts,JSx,props,state/my-app/src/App.js
@@ -7,6 +7,7 @@ function App() {
   // Let's we use context api to pass data
   // First import the context
   // Then Wrap child components in the Context Provider and supply the state value.
+  // Now, all components in this tree will have access to the user Context.
   const UserContext = createContext();
   const [count, setCount] = useState(0);
   return (
