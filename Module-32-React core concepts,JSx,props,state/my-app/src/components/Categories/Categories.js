a3b071631bb84f0c9ad146e42f89024ffbb9ea17
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-22 20:50:04 +0600

    Connect category details with cateogries

--- Module-32-React core concepts,JSx,props,state/my-app/src/components/Categories/Categories.js
+++ Module-32-React core concepts,JSx,props,state/my-app/src/components/Categories/Categories.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import CategoryDetails from '../CategoryDetails/CategoryDetails';
 
 const Categories = () => {
     return (
         <div>
             <h1>Category  </h1>
+            <CategoryDetails/>
         </div>
     );
 };
