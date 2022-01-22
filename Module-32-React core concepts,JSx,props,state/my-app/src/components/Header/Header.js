43079f5748a2c7b5d153f8ec0590666f741f9690
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-22 21:15:14 +0600

    accessing multiple data using useContext

--- Module-32-React core concepts,JSx,props,state/my-app/src/components/Header/Header.js
+++ Module-32-React core concepts,JSx,props,state/my-app/src/components/Header/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useContext } from 'react/cjs/react.development';
+import { CountContext } from '../../App';
 
 const Header = () => {
+    const [count, setCount] = useContext(CountContext);
     return (
         <div>
             <h1>Count in Header: </h1>
