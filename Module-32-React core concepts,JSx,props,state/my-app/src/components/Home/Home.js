ef8911de7ec294caddd3a247af310c2b273a7f3f
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-22 18:48:54 +0600

    destructuring props

--- Module-32-React core concepts,JSx,props,state/my-app/src/components/Home/Home.js
+++ Module-32-React core concepts,JSx,props,state/my-app/src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const Home = () => {
+const Home = (props) => {
+    const {count, setCount} = props;
     return (
         <div>
             <h1>This is a home component</h1>
