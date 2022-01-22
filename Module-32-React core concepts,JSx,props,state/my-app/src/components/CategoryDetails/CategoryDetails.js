d92eedebdef1729aa161dbdc926f5e14afe8a05c
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-22 22:27:43 +0600

    destructuring props

--- Module-32-React core concepts,JSx,props,state/my-app/src/components/CategoryDetails/CategoryDetails.js
+++ Module-32-React core concepts,JSx,props,state/my-app/src/components/CategoryDetails/CategoryDetails.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { useContext } from 'react/cjs/react.development';
 import { CountContext } from '../../App';
 
-const CategoryDetails = () => {
-    const count = useContext(CountContext)
+const CategoryDetails = (props) => {
+    const count = useContext(CountContext);
+    const {name, category} = props.product;
     return (
         <div>
             <h1>Category Details of :{count}</h1>
