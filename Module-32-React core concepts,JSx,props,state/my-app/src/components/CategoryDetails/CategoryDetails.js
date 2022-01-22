7a3baea7473838c640d074600ae78378084e0718
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-01-22 21:01:42 +0600

    import countContext using context api

--- Module-32-React core concepts,JSx,props,state/my-app/src/components/CategoryDetails/CategoryDetails.js
+++ Module-32-React core concepts,JSx,props,state/my-app/src/components/CategoryDetails/CategoryDetails.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useContext } from 'react/cjs/react.development';
-import { UserContext } from '../../App';
+import { CountContext } from '../../App';
 
 const CategoryDetails = () => {
-    const machine = useContext(UserContext)
+    const count = useContext(CountContext)
     return (
         <div>
             <h1>Category Details of :{machine}</h1>
