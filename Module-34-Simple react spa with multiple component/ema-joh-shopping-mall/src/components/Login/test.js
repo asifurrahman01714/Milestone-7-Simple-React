0688ed5f1a615ccba65d6cff72b082ff0a061b66
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-04-12 19:33:26 +0600

    Array problem

--- Module-34-Simple react spa with multiple component/ema-joh-shopping-mall/src/components/Login/test.js
+++ Module-34-Simple react spa with multiple component/ema-joh-shopping-mall/src/components/Login/test.js
@@ -1,17 +1,22 @@
 const name ="asi f";
 const nameToArrayConvertion = name.split("");
 
+// Converting the string to an Array
 console.log(nameToArrayConvertion);
 
+// Declaring a blank Array
 const newArray=[];
 for (let i = (nameToArrayConvertion.length - 1); i > -1; i--) {
     const element = nameToArrayConvertion[i];
     if(element !== " "){
+        // Pushing or Input the element to the blank newArray reversely
         newArray.push(element);
     }
 }
+// Checking the newArray. Now there is no space in the array
 console.log(newArray);
 
+// Converting array elements to lower case
 const myNumber = newArray[1].toLowerCase();
 console.log(myNumber)
 if(myNumber === "a" || myNumber === "e" || myNumber === "i" || myNumber === "o" || myNumber === "u"){
